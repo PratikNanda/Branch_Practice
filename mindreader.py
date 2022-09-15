c59043da8d7a8e325d38184e4eb02ddbea097834
PratikNanda <pratik.nanda99@gmail.com>
2022-09-15 18:50:46 +0530

    modified code

--- mindreader.py
+++ mindreader.py
@@ -1,1 +1,11 @@
-print("new branch")
+def outer_function():
+    print(1)
+    def inner_function():
+        print('in')
+    print('before call in')
+    inner_function()
+    print('out')
+
+
+outer_function()
+print('normal')
